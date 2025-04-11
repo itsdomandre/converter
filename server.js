57e479e209c55e07635b33b0e7be60132ebff9ee
Domingos André <domandreqs@gmail.com>
2025-04-11 21:46:01 +0100

    server node.js

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const ytdl = require('ytdl-core');
+const fs = require('fs');
+const app = express();
+const port = 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Endpoint para converter o link do YouTube em MP3
+app.post('/download', async (req, res) => {
+  const videoUrl = req.body.url;
+  
+  if (!ytdl.validateURL(videoUrl)) {
+    return res.status(400).send('URL inválida do YouTube');
+  }
+
+  try {
+    const info = await ytdl.getInfo(videoUrl);
+    const audioStream = ytdl(videoUrl, { filter: 'audioonly' });
+    
+    const fileName = `${info.videoDetails.title}.mp3`.replace(/[\/\\?%*:|"<>\.]/g, '-');  // Para evitar caracteres inválidos no nome do arquivo
+
+    res.header('Content-Disposition', `attachment; filename="${fileName}"`);
+    res.header('Content-Type', 'audio/mpeg');
+    
+    audioStream.pipe(res);
+  } catch (error) {
+    console.error('Erro ao baixar vídeo:', error);
+    res.status(500).send('Erro ao processar o vídeo');
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
